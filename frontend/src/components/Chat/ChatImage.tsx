82603283d42d3f47f08320dab9af565adf6a3bb6
Raghul Prasanth <87120429+Raghul-18@users.noreply.github.com>
2024-08-21 20:46:27 +0530

    Update ChatImage.tsx

--- frontend/src/components/Chat/ChatImage.tsx
+++ frontend/src/components/Chat/ChatImage.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function ChatImage({ imgURL }: { imgURL: string }) {
   return (
     <div className='w-36 sm:w-44 flex justify-center items-center'>
-      <img src={imgURL} className='object-contain' />
+      <Image src={imgURL} className='object-contain' />
     </div>
   );
 }
