c10f8549e8710c2c7f81476073c1daf4e1269528
Raghul Prasanth <87120429+Raghul-18@users.noreply.github.com>
2024-08-21 21:10:51 +0530

    Update Popup.tsx

--- frontend/src/components/shared/Popup.tsx
+++ frontend/src/components/shared/Popup.tsx
@@ -7,11 +7,15 @@ function Popup({
 }: {
   text: string;
   showPopup: boolean;
-  setShowPopup: React.Dispatch<boolean>;
+  setShowPopup: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
   useEffect(() => {
-    if (showPopup) setTimeout(() => setShowPopup(false), 3000);
-  }, [setShowPopup]);
+    if (showPopup) {
+      const timer = setTimeout(() => setShowPopup(false), 3000);
+      return () => clearTimeout(timer); // Clear timeout if the component unmounts or showPopup changes
+    }
+  }, [showPopup, setShowPopup]);
+
   return (
     <div
       className={`absolute bottom-20 left-1/2 z-30 -translate-x-1/2 opacity-0 ${
