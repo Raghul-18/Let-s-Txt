5fa8157b0b8584fc51602ee4faa4843e058ff16e
Raghul Prasanth <87120429+Raghul-18@users.noreply.github.com>
2024-08-21 20:42:57 +0530

    Update page.tsx

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function page() {
   return (
     <div className="flex justify-center items-center w-full text-xl">
-      Select a room to do Let's Txt
+      Select a room to do Let&#39;s Txt
     </div>
   );
 }
