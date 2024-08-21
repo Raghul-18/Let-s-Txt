1354cddcafd27f582897823c2faaa6058d471233
Raghul Prasanth <87120429+Raghul-18@users.noreply.github.com>
2024-08-21 21:02:19 +0530

    Update page.tsx

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-function page() {
+function Page() {
   return (
     <div className="flex justify-center items-center w-full text-xl">
-      Select a room to do Let&#39;s Txt
+      {`Select a room to do Let's Txt`}
     </div>
   );
 }
 
-export default page;
+export default Page;
