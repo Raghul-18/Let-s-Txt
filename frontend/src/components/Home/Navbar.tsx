afde2c65add2751945d29524f2542ee78c456755
Raghul Prasanth <raghul01020405@gmail.com>
2024-09-04 21:18:56 +0530

    Update Navbar.tsx

--- frontend/src/components/Home/Navbar.tsx
+++ frontend/src/components/Home/Navbar.tsx
@@ -4,9 +4,6 @@ import Image from "next/image";
 import Link from "next/link";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
-const NAV_LINKS = [
-  { title: "Star on GitHub", link: "https://github.com/diwash007/kurakani" },
-];
 
 function Navbar() {
   const [navbarActive, setNavbarActive] = useState(false);
@@ -15,13 +12,7 @@ function Navbar() {
     <div className="flex fixed justify-between items-center px-5 lg:px-36 w-screen h-[100px] bg-white">
       <Image src="/images/logo.png" alt="logo" height={60} width={80} />
       <div className="hidden gap-10 font-medium lg:flex">
-        {NAV_LINKS.map((item, index) => {
-          return (
-            <Link href={item.link} key={index} target="_blank">
-              {item.title}
-            </Link>
-          );
-        })}
+
       </div>
       <div
         className="flex gap-10 font-medium cursor-pointer lg:hidden"
@@ -35,13 +26,6 @@ function Navbar() {
       </div>
       {navbarActive && (
         <div className="text-white absolute top-[80px] bg-gradient-to-r from-purple-500 to-pink-500 right-6 rounded-full font-medium text-xl pt-2 px-4">
-          {NAV_LINKS.map((item, index) => {
-            return (
-              <Link href={item.link} key={index} target="_blank">
-                {item.title}
-              </Link>
-            );
-          })}
         </div>
       )}
     </div>
